f28b82a93382a2be3131a2bc8adea853e9d975a3
Paul CARTERON <carteronpaul@gmail.com>
2022-01-04 11:49:58 +0100

    Add lintr to continuous integration and also used it on package

--- R/get_wfs_layer.R
+++ R/get_wfs_layer.R
@@ -1,10 +1,16 @@
 #' Download WFS layer
 #'
-#' Directly download a layer from the French National Institute of Geographic and Forestry. To do that, it need a location giving by a shapfile, an apikey thanks to get_apikey and the name of layer thanks to get_layer_metadata,
+#' Directly download a layer from the French National Institute of
+#' Geographic and Forestry. To do that, it need a location giving by
+#' a shapfile, an apikey thanks to get_apikey and the name of layer
+#' thanks to get_layer_metadata.
 #'
 #' @param shape Object of class sf. Needs to be located in France
-#' @param apikey API key from get_apikey() (or directly from the website https://geoservices.ign.fr/services-web-experts)
-#' @param layer_name Name of the layer from get_layers_metadata(apikey, "wfs) (or directly from the website https://geoservices.ign.fr/services-web-experts-addAPIkey
+#' @param apikey API key from get_apikey() (or directly from the website
+#' https://geoservices.ign.fr/services-web-experts)
+#' @param layer_name Name of the layer from get_layers_metadata(apikey, "wfs)
+#' (or directly from the website :
+#' https://geoservices.ign.fr/services-web-experts-addAPIkey)
 #' @export
 #'
 #' @importFrom sf st_bbox st_transform st_make_valid st_read st_as_sf
@@ -12,56 +18,71 @@
 #' @importFrom dplyr select
 #' @importFrom magrittr `%>%`
 
-get_wfs_layer = function(shape, apikey = "cartovecto", layer_name = "BDCARTO_BDD_WLD_WGS84G:troncon_route"){
+get_wfs_layer <- function(shape,
+                          apikey = "cartovecto",
+                          layer_name = "BDCARTO_BDD_WLD_WGS84G:troncon_route") {
+  bbox <- NULL
 
-   bbox <- NULL
+  lapply_function <- function(startindex, nb_request, apikey,
+                              layer_name, shape) {
+    cat("Request ", startindex + 1, "/", nb_request + 1,
+        " downloading...\n", sep = "")
+     res <- st_read(format_url(apikey, layer_name, shape,
+                               startindex = 1000 * startindex),
+                    quiet = TRUE)
+  }
 
-   lapply_function = function(startindex, nb_request, apikey, layer_name, shape){
-      cat("Request ",startindex+1,"/",nb_request+1," downloading...\n", sep = "")
-      res = st_read(format_url(apikey, layer_name, shape, startindex = 1000*startindex), quiet = TRUE)
-   }
+  shape <- st_make_valid(shape) %>%
+    st_transform(4326)
 
-   shape = st_make_valid(shape) %>%
-      st_transform(4326)
+  resp <- GET(format_url(apikey, layer_name, shape, startindex = 0))
 
-   resp = GET(format_url(apikey, layer_name, shape, startindex = 0))
+  stop_for_status(resp,
+                  task = paste0("find ressource. Check layer_name ",
+                                "at https://geoservices.ign.fr/,",
+                                "services-web-experts-",
+                                apikey))
 
-   stop_for_status(resp, task = paste0("find ressource. Check layer_name at https://geoservices.ign.fr/services-web-experts-",apikey))
+  nb_features <- content(resp)$numberMatched
+  if (nb_features == 0) {
+    stop(paste("Ressource doesn't exist. Check the shape,",
+               "it's probably out of France"))
+  }
+  nb_request <- nb_features %/% 1000
 
-   nb_features = content(resp)$numberMatched
-   if (nb_features == 0){stop("Ressource doesn't exist. Check the shape, it's probably out of France")}
-   nb_request = nb_features %/% 1000
-
-   result = lapply(X = 0:nb_request,
-                   FUN = lapply_function,
-                   nb_request = nb_request,
-                   apikey = apikey,
-                   layer_name = layer_name,
-                   shape = shape) %>%
-      as.data.frame() %>%
-      st_as_sf() %>%
-      st_make_valid() %>%
-      select(-bbox)
+  result <- lapply(
+    X = 0:nb_request,
+    FUN = lapply_function,
+    nb_request = nb_request,
+    apikey = apikey,
+    layer_name = layer_name,
+    shape = shape
+  ) %>%
+    as.data.frame() %>%
+    st_as_sf() %>%
+    st_make_valid() %>%
+    select(-bbox)
 }
 
-format_bbox = function(shape = NULL){
-   bbox = st_bbox(shape)
-   paste(bbox["xmin"],bbox["ymin"],bbox["xmax"],bbox["ymax"],"epsg:4326", sep = ",")
+format_bbox <- function(shape = NULL) {
+  bbox <- st_bbox(shape)
+  paste(bbox["xmin"], bbox["ymin"], bbox["xmax"], bbox["ymax"], "epsg:4326",
+        sep = ",")
 }
-format_url = function(apikey = NULL, layer_name = NULL, shape = NULL, startindex = NULL){
-
-   url <- modify_url("https://wxs.ign.fr",
-                     path = paste0(apikey,"/geoportail/wfs"),
-                     query = list(SERVICE = "WFS",
-                                  VERSION = "2.0.0",
-                                  REQUEST = "GetFeature",
-                                  outputFormat = "json",
-                                  srsName = "EPSG:4326",
-                                  typeName = layer_name,
-                                  bbox = format_bbox(st_transform(shape,4326)),
-                                  startindex = startindex))
-   url
+format_url <- function(apikey = NULL, layer_name = NULL,
+                       shape = NULL, startindex = NULL) {
+  url <- modify_url("https://wxs.ign.fr",
+    path = paste0(apikey, "/geoportail/wfs"),
+    query = list(
+      SERVICE = "WFS",
+      VERSION = "2.0.0",
+      REQUEST = "GetFeature",
+      outputFormat = "json",
+      srsName = "EPSG:4326",
+      typeName = layer_name,
+      bbox = format_bbox(st_transform(shape, 4326)),
+      startindex = startindex
+    )
+  )
+  url
 }
-
-
-
--- tests/testthat/test-get_wfs_layer.R
+++ tests/testthat/test-get_wfs_layer.R
@@ -7,23 +7,23 @@ test_that("format_bbox return expected format", {
    expect_error(format_bbox())
 
    # Test for point
-   point = st_point(1:3)
+   point <- st_point(1:3)
    expect_match(format_bbox(point),
                 "(\\d*\\.\\d*+,)*+epsg:4326",
                 perl = TRUE)
 
    # Test for multipoint
-   multipoint = st_multipoint(matrix(1:10,5))
+   multipoint <- st_multipoint(matrix(1:10, 5))
    expect_match(format_bbox(multipoint),
                 "(\\d*\\.\\d*+,)*+epsg:4326",
                 perl = TRUE)
 
    # Test for polygon
-   outer = matrix(c(0,0,10,0,10,10,0,10,0,0),ncol=2, byrow=TRUE)
-   hole1 = matrix(c(1,1,1,2,2,2,2,1,1,1),ncol=2, byrow=TRUE)
-   hole2 = matrix(c(5,5,5,6,6,6,6,5,5,5),ncol=2, byrow=TRUE)
-   pts = list(outer, hole1, hole2)
-   polygon = st_polygon(pts)
+   outer <- matrix(c(0, 0, 10, 0, 10, 10, 0, 10, 0, 0), ncol = 2,  byrow = TRUE)
+   hole1 <- matrix(c(1, 1, 1, 2, 2, 2, 2, 1, 1, 1), ncol = 2,  byrow = TRUE)
+   hole2 <- matrix(c(5, 5, 5, 6, 6, 6, 6, 5, 5, 5), ncol = 2,  byrow = TRUE)
+   pts <- list(outer, hole1, hole2)
+   polygon <- st_polygon(pts)
    expect_match(format_bbox(polygon),
                 "(\\d*\\.\\d*+,)*+epsg:4326",
                 perl = TRUE)
@@ -31,12 +31,13 @@ test_that("format_bbox return expected format", {
 test_that("format_url return good url", {
    expect_error(format_url())
 
-   apikey = "apikey"
-   layer_name = "layer_name"
-   startindex = 1
+   apikey <- "apikey"
+   layer_name <- "layer_name"
+   startindex <- 1
 
-   shape = st_polygon(list(matrix(c(0,0,10,0,10,10,0,10,0,0),ncol=2, byrow=TRUE)))
-   shape = st_sfc(shape)
+   shape <- st_polygon(list(matrix(c(0, 0, 10, 0, 10, 10, 0, 10, 0, 0),
+                                   ncol = 2, byrow = TRUE)))
+   shape <- st_sfc(shape)
    st_crs(shape) <- st_crs(4326)
 
    expect_false(is.na(st_crs(shape)))
@@ -47,24 +48,45 @@ test_that("format_url return good url", {
 })
 test_that("get_wfs_layer API is valid", {
    # example shape for testing
-   shape = st_polygon(list(matrix(c(-4.373,-4.373,-4.372,-4.372,-4.373,47.798,47.799,47.799,47.798,47.798), ncol = 2)))
-   shape = st_sfc(shape, crs = st_crs(4326))
+   shape <- st_polygon(list(matrix(c(-4.373, -4.373,
+                                     -4.372, -4.372,
+                                     -4.373, 47.798,
+                                     47.799, 47.799,
+                                     47.798, 47.798),
+                                   ncol = 2)))
+   shape <- st_sfc(shape, crs = st_crs(4326))
+
+   layer_name <- "BDCARTO_BDD_WLD_WGS84G:troncon_route"
+   apikey <- "cartovecto"
    use_cassette("get_wfs_layer", {
-      layer <- get_wfs_layer(shape, apikey = "cartovecto", layer_name = "BDCARTO_BDD_WLD_WGS84G:troncon_route")
+      layer <- get_wfs_layer(shape = shape,
+                             apikey = apikey,
+                             layer_name = layer_name)
    })
    expect_s3_class(layer, "sf")
 })
 test_that("get_wfs_layer errors when the API doesn't behave", {
    # example shape for testing
-   shape = st_polygon(list(matrix(c(-4.373,-4.373,-4.372,-4.372,-4.373,47.798,47.799,47.799,47.798,47.798), ncol = 2)))
-   shape = st_sfc(shape, crs = st_crs(4326))
+   shape <- st_polygon(list(matrix(c(-4.373, -4.373,
+                                     -4.372, -4.372,
+                                     -4.373, 47.798,
+                                     47.799, 47.799,
+                                     47.798, 47.798),
+                                   ncol = 2)))
+   shape <- st_sfc(shape, crs = st_crs(4326))
+   url <- paste0("https://wxs.ign.fr/cartovecto/geoportail/wfs?",
+                 "SERVICE=WFS&",
+                 "VERSION=2.0.0&",
+                 "REQUEST=GetFeature&",
+                 "outputFormat=json&",
+                 "srsName=EPSG%3A4326&",
+                 "typeName=BDCARTO_BDD_WLD_WGS84G%3Atroncon_route&",
+                 "bbox=-4.373%2C47.798%2C-4.372%2C47.799%2Cepsg%3A4326&",
+                 "startindex=0")
    enable()
    stub <- stub_request("get",
-                                  "https://wxs.ign.fr/cartovecto/geoportail/wfs?SERVICE=WFS&VERSION=2.0.0&REQUEST=GetFeature&outputFormat=json&srsName=EPSG%3A4326&typeName=BDCARTO_BDD_WLD_WGS84G%3Atroncon_route&bbox=-4.373%2C47.798%2C-4.372%2C47.799%2Cepsg%3A4326&startindex=0")
+                       url)
    to_return(stub, status = 503)
    expect_error(get_wfs_layer(shape), regexp = "Check layer_name", fixed = TRUE)
    disable()
 })
-
-
-
