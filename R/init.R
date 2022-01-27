08f595389b5a3db505fccbd6f3dfa50a9e56a1b9
Paul CARTERON <carteronpaul@gmail.com>
2022-01-27 12:01:47 +0100

    Try to recreate error with curk fetch memory

--- R/init.R
+++ R/init.R
@@ -9,14 +9,15 @@
       stop("No internet connection.")
    }
 
-   # base_url <- "http://geoservices.ign.fr/"
-   # # check that IGN web service can be reached
-   # if (http_error(base_url)) { # TRUE: 400 or above
-   #    packageStartupMessage("IGN web service API is unavailable.\n",
-   #                          "It may be due to a site crash ",
-   #                          "or an update of the IGN data. More information at ",
-   #                          "<https://geoservices.ign.fr/actualites>.")
-   # }else{
-   #    packageStartupMessage("IGN web service API is available.")
-   # }
+   base_url <- "http://geoservices.ign.fr/"
+
+   # check that IGN web service can be reached
+   if (http_error(base_url)) { # TRUE: 400 or above
+      packageStartupMessage("IGN web service API is unavailable.\n",
+                            "It may be due to a site crash ",
+                            "or an update of the IGN data. More information at ",
+                            "<https://geoservices.ign.fr/actualites>.")
+   }else{
+      packageStartupMessage("IGN web service API is available.")
+   }
 }
