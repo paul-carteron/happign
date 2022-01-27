bf38e40bda050390b6b7559b9e790d812cb57691
Paul CARTERON <carteronpaul@gmail.com>
2022-01-27 10:09:35 +0100

    Removing call to curl lib to test if problem to install dependency come from him

--- R/init.R
+++ R/init.R
@@ -1,19 +1,16 @@
-#' @importFrom curl has_internet
 #' @importFrom httr http_error
 #'
 #' @export
 #'
 .onAttach <- function(libname, pkgname) {
 
-   # check Internet connection
-   if (!curl::has_internet()) {
-      packageStartupMessage("No Internet connection.")
-   }
-
    base_url <- "http://geoservices.ign.fr/"
    # check that iNat can be reached
-   if (httr::http_error(base_url)) { # TRUE: 400 or above
-      packageStartupMessage("IGN web service API is unavailable.")
+   if (http_error(base_url)) { # TRUE: 400 or above
+      packageStartupMessage("IGN web service API is unavailable.\n",
+                            "It may be due to a site crash ",
+                            "or an update of the IGN data. More information at ",
+                            "<https://geoservices.ign.fr/actualites>.")
    }else{
       packageStartupMessage("IGN web service API is available.")
    }
