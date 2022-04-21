a19937b67e54b8766dfb73f1ab254171af922d72
Paul CARTERON <carteronpaul@gmail.com>
2022-04-21 21:17:39 +0200

    Twist to allow NULL argument for partition

--- R/get_apicarto_gpu.R
+++ R/get_apicarto_gpu.R
@@ -1,6 +1,9 @@
-#' Apicarto module Géoportail de l'urbanisme
+#' Apicarto module Geoportail de l'urbanisme
 #'
-#' TO DO DESCRIPTION & USAGE
+#' @usage
+#' get_apicarto_plu (x,
+#'                   ressource = "zone-urba",
+#'                   partition = NULL)
 #'
 #' @param x An object of clas `sf` or `sfc`. If NULL, `partition` must be filled by partition of PLU.
 #' @param ressource A character from this list : "document", "zone-urba", "secteur-cc", "prescription-surf",
@@ -21,9 +24,9 @@
 #' * `"info-lin"` :
 #' * `"info-pct"` :
 #'
-#' @importFrom checkmate assert assert_choice check_character check_class
+#' @importFrom checkmate assert assert_choice check_character check_class check_null
 #' @importFrom sf read_sf
-#' @importFrom httr2 req_perform req_url_path_append req_url_query req_user_agent request resp_body_json
+#' @importFrom httr2 req_perform req_url_path_append req_url_query req_user_agent request resp_body_json resp_body_string
 #'
 #' @return A object of class `sf`
 #' @export
@@ -33,29 +36,70 @@
 #' library(tmap)
 #' library(sf)
 #' point <- st_sfc(st_point(c(-0.4950188466302029, 45.428039987269926)), crs = 4326)
+#'
+#' # If you know the partition
+#' partition <- "DU_17345"
+#' poly <- get_apicarto_plu(x = NULL, ressource = "zone-urba", partition = partition)
+#' qtm(poly)+qtm(point, symbols.col = "red", symbols.size = 2)
+#'
+#' # If you don't know partition
+#' poly <- get_apicarto_plu(x = point, ressource = "zone-urba", partition = "auto")
+#' qtm(poly)+qtm(point, symbols.col = "red", symbols.size = 2)
+#'
+#' # If you wanna find partition
+#' document <- get_apicarto_plu(point, ressource = "document", partition = NULL)
+#' partition <- unique(document$partition)
+#'
+#' # If you want only intersection between you're shape and PLU
+#' poly <- get_apicarto_plu(point, ressource = "zone-urba", partition = NULL)
+#' qtm(poly)+qtm(point, symbols.col = "red", symbols.size = 2)
+#'
+#' # Get all prescription : /!\ prescription is different than zone-urba
+#'
+#' partition <- "DU_17345"
+#' ressources <- c("prescription-surf", "prescription-lin", "prescription-pct")
+#' res <- list()
+#' for (i in 1:3){
+#'     res_temp <- get_apicarto_plu(x, ressources[i], partition)
+#'     res[[i]] <- res_temp
 #' }
 #'
-get_apicarto_plu <- function(x = NULL,
+#'
+#' }
+#'
+#'
+get_apicarto_plu <- function(x,
                              ressource = "zone-urba",
-                             partition = "auto"){
+                             partition = NULL){
 
    # Test input values
    assert(check_class(x, "sf"),
-          check_class(x, "sfc"))
+          check_class(x, "sfc"),
+          check_null(x))
    assert(check_character(partition, pattern = "auto"),
-          check_character(partition, pattern = "(?:DU|PSMW)_(?:[0-9]{5})$"))
-   assert_choice(ressource, c("zone-urba", "secteur-cc", "prescription-surf",
+          check_character(partition, pattern = "(?:DU|PSMW)_(?:[0-9]{5})$"),
+          check_null(partition))
+   assert_choice(ressource, c("document","zone-urba", "secteur-cc", "prescription-surf",
                                "prescription-lin", "prescription-pct",
                                "info-surf", "info-lin", "info-pct"))
 
-   if (partition == "auto"){
+
+   find_PLU_partition <- function(geom){
       resp <- request("https://apicarto.ign.fr/api/gpu/document") %>%
          req_url_query(geom = shp_to_geojson(x)) %>%
          req_perform() %>%
          resp_body_json()
 
       partition <- resp[["features"]][[1]][["properties"]][["partition"]]
+
    }
+
+   # Avoid problem when partition == NULL in if statement
+   partition <- switch(class(partition),
+                       "NULL" = NULL,
+                       "character" = ifelse(partition == "auto",
+                                            find_PLU_partition(x),
+                                            partition))
 
    if (is.character(partition)){
       x <- NULL
@@ -65,8 +109,10 @@ get_apicarto_plu <- function(x = NULL,
    request <- request("https://apicarto.ign.fr/api/gpu") %>%
       req_user_agent("happign (https://paul-carteron.github.io/happign/)") %>%
       req_url_path_append(ressource) %>%
-      req_url_query(partition = partition, geom = shp_to_geojson(x))
-
-   shp <- read_sf(request$url)
+      req_url_query(partition = partition, geom = shp_to_geojson(x)) %>%
+      req_perform() %>%
+      resp_body_string() %>%
+      read_sf()
 
 }
+
