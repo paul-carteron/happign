dfa29b3593bffbfd5a73aa5a3015011b5398233f
Paul CARTERON <carteronpaul@gmail.com>
2022-05-25 16:07:12 +0200

    Correct vignettes Fix vcr cassettes

--- tests/testthat/setup-happign.R
+++ tests/testthat/setup-happign.R
@@ -1,5 +1,5 @@
-library("vcr") # *Required* as vcr is set up on loading
+library(vcr)
 invisible(vcr::vcr_configure(
-  dir = vcr::vcr_test_path("fixtures")
+   dir = vcr::vcr_test_path("fixtures")
 ))
 vcr::check_cassette_names()
