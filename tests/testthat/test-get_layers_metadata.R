53a5bac604d6bb35dc30d9c210b3fa97b8ef87fe
Paul CARTERON <carteronpaul@gmail.com>
2022-05-28 11:06:35 +0200

    Implementation of httptest2 for get_layers_metadata

--- tests/testthat/test-get_layers_metadata.R
+++ tests/testthat/test-get_layers_metadata.R
@@ -24,29 +24,28 @@ test_that("xml_to_df", {
    )
 })
 
-test_that("get_layers_metada_wfs", {
+with_mock_dir("get_layers_metada_wfs", {
+   test_that("get_layers_metada_wfs", {
 
-   vcr::use_cassette("get_layers_metada_wfs", {
       res <- get_layers_metadata("altimetrie", "wfs")
-   })
 
-   expect_equal(dim(res), c(1, 7))
-   expect_equal(names(res),
-                c("itemindex", "name", "title", "abstract",
-                  "keywords", "defaultcrs", "wgs84boundingbox"))
+      expect_equal(dim(res), c(1, 7))
+      expect_equal(names(res),
+                   c("itemindex", "name", "title", "abstract",
+                     "keywords", "defaultcrs", "wgs84boundingbox"))
+      })
 })
 
+with_mock_dir("get_layers_metada_wms", {
+   # /!\ you have to manually change encoding to "ISO-8859-1" inside .xml or .R file from mocking
+   test_that("get_layers_metada_wms", {
 
-test_that("get_layers_metada_wms", {
-
-   vcr::use_cassette("get_layers_metada_wms", {
       res <- get_layers_metadata("ortho", "wms")
-   })
 
-   expect_equal(dim(res), c(12, 12))
-   expect_equal(names(res),
-                c("itemindex", "name", "title", "abstract", "keywordlist",
-                  "crs", "ex_geographicboundingbox", "boundingbox",
-                  "metadataurl", "style", "minscaledenominator",
-                  "maxscaledenominator"))
+      expect_equal(dim(res), c(12, 12))
+      expect_equal(names(res), c("itemindex", "name", "title", "abstract", "keywordlist",
+                                 "crs", "ex_geographicboundingbox", "boundingbox",
+                                 "metadataurl", "style", "minscaledenominator",
+                                 "maxscaledenominator"))
+      })
 })
