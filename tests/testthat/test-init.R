e7ec5eea4c48ae2262d572bddcb0d96458471889
Paul CARTERON <carteronpaul@gmail.com>
2022-06-05 10:50:21 +0200

    test onAttach with httptest2

--- tests/testthat/test-init.R
+++ tests/testthat/test-init.R
@@ -1,5 +1,19 @@
-test_that(".onAttach() with internet connection", {
-   skip_if_offline()
-   expect_message(happign:::.onAttach(), regexp = "IGN", fixed = TRUE)
-   expect_message(happign:::.onAttach(), regexp = "Last news", fixed = TRUE)
-})
+with_mock_dir("onAttach", {
+   #/!\ Again, you have to manually change encoding "UTF-8" to "ISO-8859-1" !
+   test_that("onAttach", {
+      skip_on_cran()
+      skip_if_offline()
+
+      expect_message(happign:::.onAttach(), "Last news from IGN website")
+      expect_message(happign:::.onAttach(), "IGN web service API is available")
+   })
+}, simplify = FALSE)
+with_mock_dir("onAttach error", {
+   #/!\ Again, you have to manually change encoding "UTF-8" to "ISO-8859-1" !
+   test_that("onAttach", {
+      skip_on_cran()
+      skip_if_offline()
+
+      expect_message(happign:::.onAttach(), "is unavailable")
+   })
+}, simplify = FALSE)
