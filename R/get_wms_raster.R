75cadb6d40486669e79261a2ba0950f6a4b99ffc
Paul CARTERON <carteronpaul@gmail.com>
2022-10-04 07:37:32 +0200

    Little bit of blush

--- R/get_wms_raster.R
+++ R/get_wms_raster.R
@@ -279,8 +279,8 @@ download_tiles <- function(urls, crs, format) {
    options("timeout" = 3600)
    on.exit(options(default))
 
-   default_gdal_skip <- Sys.getenv(GDAL_SKIP)
-   default_gdal_http_unsafessl <- Sys.getenv(GDAL_HTTP_UNSAFESSL)
+   default_gdal_skip <- Sys.getenv("GDAL_SKIP")
+   default_gdal_http_unsafessl <- Sys.getenv("GDAL_HTTP_UNSAFESSL")
    Sys.setenv(GDAL_SKIP="DODS")
    Sys.setenv(GDAL_HTTP_UNSAFESSL="YES")
    on.exit(Sys.setenv(GDAL_SKIP=default_gdal_skip))
