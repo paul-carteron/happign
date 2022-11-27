84e8f34ccc0dce29a27f34d827dcee6afbb59f0e
Paul CARTERON <carteronpaul@gmail.com>
2022-11-27 14:01:04 +0100

    complete test for get_last_news

--- tests/testthat/test-get_last_news.R
+++ tests/testthat/test-get_last_news.R
@@ -4,3 +4,10 @@ with_mock_dir("get_last_news", {
       expect_message(get_last_news(), "Last news from IGN website")
    })
 }, simplify = FALSE)
+
+with_mock_dir("get_last_news_error", {
+   test_that("get_last_news_error", {
+      # get_last_news_error moxk is modify to respond 404 error
+      expect_message(get_last_news(), "IGN actuality is unavailable")
+   })
+}, simplify = FALSE)
