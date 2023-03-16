22bcbf67fab5bc2838cf44160e6165fda448e72d
Paul CARTERON <carteronpaul@gmail.com>
2023-03-16 17:29:06 +0100

    add get_apicarto_code_postaux

---
+++ R/get_apicarto_codes_postaux.R
@@ -0,0 +1,49 @@
+#' Apicarto Codes Postaux
+#'
+#' Implementation of the "Codes Postaux" module from the
+#'  [IGN's apicarto](https://apicarto.ign.fr/api/doc/codes-postaux). This
+#'  API give information about commune from postal code.
+#'
+#' @usage
+#' get_apicarto_codes_postaux(code_post)
+#'
+#' @param code_post `character` corresponding to the postal code of a commune
+#'
+#' @importFrom httr2 req_perform resp_body_json
+#'
+#' @return Object of class `data.frame`
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#'
+#' info_commune <- get_apicarto_codes_postaux("29760")
+#'
+#' code_post <- c("29760", "29260")
+#' info_communes <- get_apicarto_codes_postaux(code_post)
+#'}
+#'
+#' @name get_apicarto_codes_postaux
+#' @export
+#'
+
+get_apicarto_codes_postaux <- function(code_post){
+
+   class_check(code_post, "character")
+
+   hit_code_post <- function(code_post){
+      resp <- build_req(path = paste0("api/codes-postaux/communes/",
+                                      code_post)) |>
+         req_perform() |>
+         resp_body_json()
+      resp <- do.call(rbind, resp)
+   }
+
+   resp <- Map(hit_code_post,
+               code_post)
+
+   # bind rows of each Map call
+   resp <- data.frame(do.call(rbind, resp))
+
+   return(resp)
+}
---
+++ tests/testthat/test-get_apicarto_codes_postaux.R
@@ -0,0 +1,17 @@
+test_that("error", {
+   expect_error(get_apicarto_cadastre(29760))
+})
+
+with_mock_dir("get_api_codes_postaux", {
+   test_that("get_api_codes_postaux", {
+
+      simple_req <- get_apicarto_codes_postaux("29760")
+      multi_req <- get_apicarto_codes_postaux(c("29760", "29160"))
+
+      expect_equal(dim(simple_req), c(1, 4))
+      expect_equal(class(simple_req), "data.frame")
+
+      expect_equal(dim(multi_req), c(3, 4))
+      expect_equal(class(simple_req), "data.frame")
+   })
+}, simplify = FALSE)
