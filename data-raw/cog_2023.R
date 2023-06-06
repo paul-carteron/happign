90a3b4bccab18b4b9b489ce3c05e6dd895f8800d
paul-carteron <carteronpaul@gmail.com>
2023-06-06 10:03:59 +0200

    clean happign for forester vignette

--- data-raw/cog_2023.R
+++ data-raw/cog_2023.R
@@ -1,7 +1,9 @@
 # code to prepare `cog_2023` dataset goes here
 
 # Avoid non-ascii warning by forcing to UTF-8 encode
-cog_2023 <- read.csv("https://www.insee.fr/fr/statistiques/fichier/6800675/v_commune_2023.csv")
+cog_2023 <- read.csv("https://www.insee.fr/fr/statistiques/fichier/6800675/v_commune_2023.csv") |>
+   dplyr::filter(!is.na(REG)) |>
+   dplyr::select(COM, LIBELLE)
 
 Encoding(cog_2023$LIBELLE) <- "latin1"
 cog_2023$LIBELLE <- iconv(
@@ -9,5 +11,7 @@ cog_2023$LIBELLE <- iconv(
    "latin1",
    "UTF-8"
 )
+
+
 
 usethis::use_data(cog_2023, overwrite = T)
