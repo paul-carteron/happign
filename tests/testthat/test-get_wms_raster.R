80eb384331792c52f3c54f5f42d62252c9ee8f6a
Paul Carteron <p.carteron@gourmainbarthelemy.com>
2025-01-23 17:17:16 +0100

    add test to get_wms_raster helpers

--- tests/testthat/test-get_wms_raster.R
+++ tests/testthat/test-get_wms_raster.R
@@ -90,3 +90,78 @@ test_that("wms_bad_name", {
                                overwrite = TRUE),
                 "isn't a valid layer.")
 })
+
+test_that("get_sd_work", {
+   skip_on_cran()
+   skip_if_offline()
+
+   wms_url <- get_sd(layer)
+   normal_result <- paste0(
+      "WMS:https://data.geopf.fr/wms-r/wms?",
+      "SERVICE=WMS",
+      "&VERSION=1.3.0",
+      "&REQUEST=GetMap",
+      "&LAYERS=", layer,
+      "&CRS=IGNF:WGS84G",
+      "&BBOX=-64.000000000,-23.000000000,168.000000000,52.000000000"
+   )
+   expect_equal(wms_url, normal_result)
+})
+
+test_that("generate_desc_xml_work", {
+   skip_on_cran()
+   skip_if_offline()
+
+   sd <- get_sd(layer)
+
+   tmp_xml <- generate_desc_xml(sd, rgb = T)
+   xml <- paste0(readLines(tmp_xml), collapse = "")
+   xml <- gsub(" ", "", xml)
+
+   expect_false(grepl("<DataType>Float32</DataType>", xml))
+   expect_false(grepl("<ImageFormat>image/geotiff</ImageFormat>", xml))
+
+   expect_true(grepl("<ImageFormat>image/jpeg</ImageFormat>", xml))
+   expect_true(grepl("<Version>.*<ServerUrl>.*<Layers>.*<CRS>", xml))
+
+   # also test modify_xml_for_float
+   modified_xml <- modify_xml_for_float(tmp_xml)
+   xml <- readLines(tmp_xml)
+
+   expect_true(grepl("<DataType>Float32</DataType>", xml))
+   expect_true(grepl("<ImageFormat>image/geotiff</ImageFormat>", xml))
+
+   expect_false(grepl("<ImageFormat>image/jpeg</ImageFormat>", xml))
+   expect_true(grepl("<Version>.*<ServerUrl>.*<Layers>.*<CRS>", xml))
+})
+
+test_that("create_options_work", {
+   expect_equal(
+      create_options(),
+      c(
+         "-co", "COMPRESS=DEFLATE",
+         "-co", "PREDICTOR=2",
+         "-co", "NUM_THREADS=ALL_CPUS",
+         "-co", "BIGTIFF=IF_NEEDED"
+      )
+   )
+})
+
+test_that("config_options_work", {
+   expect_equal(
+      config_options(),
+      c(
+         GDAL_SKIP = "DODS",
+         GDAL_HTTP_UNSAFESSL = "YES",
+         VSI_CACHE = "TRUE",
+         GDAL_CACHEMAX = "30%",
+         VSI_CACHE_SIZE = "10000000",
+         GDAL_HTTP_MULTIPLEX = "YES",
+         GDAL_INGESTED_BYTES_AT_OPEN = "32000",
+         GDAL_DISABLE_READDIR_ON_OPEN = "EMPTY_DIR",
+         GDAL_HTTP_VERSION = "2",
+         GDAL_HTTP_MERGE_CONSECUTIVE_RANGES = "YES",
+         GDAL_HTTP_USERAGENT = "happign (https://github.com/paul-carteron/happign)"
+      )
+   )
+})
