b3e100ed094dd2cf33b1a69eb0f2b865c0c4c13c
David Dorchies <david.dorchies@inrae.fr>
2025-07-28 17:22:49 +0200

    vignettes: precompile vignette from previous knit
    Refs #40

--- vignettes/precompile.R
+++ vignettes/precompile.R
@@ -9,9 +9,46 @@ opts_knit$set(base.dir = "vignettes", base.url = "")
 
 input <- "vignettes/getting_started.Rmd.orig"
 output <- "vignettes/getting_started.Rmd"
+
+# Convert R code into R chunks and delete chunk outputs
+lines <- readLines(output)
+state = "text"
+for (i in seq_along(lines)) {
+   l <- lines[i]
+   if (l == "``` r") {
+      l <- "```{r}"
+      state <- "chunk"
+   } else if(l == "```") {
+      if (state == "text") {
+         l <- "<!--  TO BE DELETED -->"
+         state <- "output"
+      } else if(state == "chunk") {
+         state <- "text"
+      } else if (state == "output") {
+         l <- "<!--  TO BE DELETED -->"
+         state <- "text"
+      }
+   } else if (state == "output") {
+      l <- "<!--  TO BE DELETED -->"
+   }
+   if (i > 1) {
+      if (l == "" && (lines[i - 1] == "" || lines[i - 1] == "<!--  TO BE DELETED -->")) {
+         l <- "<!--  TO BE DELETED -->"
+      }
+   }
+   lines[i] <- l
+}
+lines <- lines[lines != "<!--  TO BE DELETED -->"]
+writeLines(unlist(lines), input)
+
 knit(input, output)
 
-# Remove only the caption lines
+# Post-processing
 lines <- readLines(output)
+# Remove only the caption lines (Is it still necessary?)
 cleaned <- grep('<p class="caption">.*</p>', lines, invert = TRUE, value = TRUE)
+# Remove automated generated images
+cleaned <- cleaned[!grepl("^!\\[plot of chunk", cleaned)]
 writeLines(cleaned, output)
+# Clean folder from created images
+unlink("vignettes/figure", recursive = TRUE, force = TRUE)
